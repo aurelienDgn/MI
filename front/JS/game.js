cd6021c93af55cb95315016a32aa5941c80f2d2b
aurelienDgn <72395256+aurelienDgn@users.noreply.github.com>
2022-05-10 14:39:09 +0200

    Add files via upload
    Barre de sélection des matériaux/éléments avec save

--- front/JS/game.js
+++ front/JS/game.js
@@ -47,52 +47,77 @@ valider.addEventListener('click', function () {
 
 let tabElt = ["Murs", "Toit", "Sol", "Portes & fenêtres"];
 let imgElt = ["../Image/walls.png", "../Image/toit.png", "../Image/floor.png", "../Image/door.png"];
-let tabMatMurs = ["Bois", "Metal", "Aluminium"];
-let imgMat = ["../Image/wood.png", "../Image/wood.png", "../Image/wood.png"];
+
+// Tableaux des éléments à modifier
+let tabMatMurs = ["Retour","Bois", "Metal", "Aluminium"];
+let tabMatToit = ["Retour","Bois", "Metal", "Aluminium"];
+let tabMatSol = ["Retour","Bois", "Metal", "Aluminium"];
+let tabMatPF = ["Retour","Bois", "Metal", "Aluminium"];
+
+// Tableaux des images des matériaux des différents éléments
+let imgMatMurs = ["../Image/croix.png","../Image/wood.png", "../Image/wood.png", "../Image/wood.png"];
+let imgMatToit = ["../Image/croix.png","../Image/wood.png", "../Image/wood.png", "../Image/wood.png"];
+let imgMatSol = ["../Image/croix.png","../Image/wood.png", "../Image/wood.png", "../Image/wood.png"];
+let imgMatPF = ["../Image/croix.png","../Image/wood.png", "../Image/wood.png", "../Image/wood.png"];
 
 let barre = document.getElementById("barre");
 
 barreElt();
 
 function barreMat(mat){
-
+    // Fonction affichant les différents matériaux d'un élément de la maison que l'on peut modifier
     while(barre.firstChild){
+        // On supprime tout les enfants de la div barre
         barre.removeChild(barre.firstChild);
     }
 
-    console.log(mat);
+    // En fonction de l'élément passer en paramètre on appelle la fonction correspondante qui va afficher les matériaux disponibles
     switch (mat){
         case "Murs":
-            matElt(tabMatMurs);
+            matElt(tabMatMurs, imgMatMurs, "murs");
             break;
         case "Toit":
+            matElt(tabMatToit, imgMatToit, "toit");
             break;
         case "Sol":
+            matElt(tabMatSol, imgMatSol, "sol");
             break;
         case "Portes & fenêtres":
+            matElt(tabMatPF, imgMatPF, "pf");
             break;
     }
 }
 
-function matElt(tabMat){
+function matElt(tabMat, imgTab, elt){
+    // Fonction qui affiche les matériaux dispo en fct de l'élément passé en param
     
     for(let i=0;i<tabMat.length;i++){
+        // Création des div 
         let div = document.createElement("div");
         div.id = "elt"+i;
         div.className = "elt";
     
+        // Création de l'image
         let img = document.createElement("img");
-        img.src = imgMat[i];
+        img.src = imgTab[i];
         img.className = "img";
     
+        // Création du text
         let p = document.createElement("h4");
         p.className = "nameElt";
         p.innerText = tabMat[i];
     
+        // On ajoute texte et image à la div
         div.appendChild(img);
         div.appendChild(p);
     
+        // Et la div à la barre
         barre.appendChild(div);
+
+        div.addEventListener('click', function(){
+            // Sélectionne le matériaux cliqué avec l'elt
+            selecElt(elt, tabMat[i]);
+        });
 
         // Lance une animation de fondu 
         document.getElementById(div.id).animate([
@@ -105,46 +130,86 @@ function matElt(tabMat){
 }
 
 function barreElt(){
+    // Créer les éléments que l'on pourra modifier
 
+    // On enlève ce que contient la barre (au cas ou ou non)
     while(barre.firstChild){
         barre.removeChild(barre.firstChild);
     }
 
     for(let i=0;i<tabElt.length;i++){
+        // Création de la div
         let div = document.createElement("div");
         div.id = "elt"+i;
         div.className = "elt";
 
+
         div.addEventListener('click', function (){
+            // Lorsque l'on clique sur un élément ça appelle la fct qui montre les matériaux avec en parametre l'elt cliqué
             barreMat(tabElt[i]);
         });
     
+        // Création de l'img
         let img = document.createElement("img");
         img.src = imgElt[i];
         img.className = "img";
     
+        // Création du texte
         let p = document.createElement("h4");
         p.className = "nameElt";
         p.innerText = tabElt[i];
     
+        // Ajoute tout ça dans la div
         div.appendChild(img);
         div.appendChild(p);
     
         barre.appendChild(div);
 
-        // Lance une animation de fondu 
-        let d = document.getElementById("test");
-        d.addEventListener('click', function(){
-            myMove();
-        });
+        
+        // Petite animation du pauvre
+        let k = document.getElementById(div.id);
+        k.animate([
+            { opacity: 0 },
+            { opacity: 1 }
+        ], {
+            duration: 1500
+        })
+       /* k.style.left = "30em";
+        k.style.position = "relative";
 
-        let ttt = document.getElementById("elt3");
-        
-        
+        function delay(n){
+            return new Promise(function(resolve){
+                setTimeout(resolve,n*1000);
+            });
+        }
+
+        let ppp = 30;
+
+        async function myAsyncFunction(){
+            for(let i=0;i<60;i++){
+                console.log("haaa");
+                k.style.left = ppp+"em";
+                ppp -= 0.5;
+                await delay(0.01);
+
+            }
+        }
+
+        myAsyncFunction();   */
     }
-
 }
 
+
+function selecElt(elt, mat){
+    console.log(elt+", "+mat);
+
+    switch(mat){
+        case "Retour":
+            barreElt();
+            break;
+        
+    }
+}
 
 
 
